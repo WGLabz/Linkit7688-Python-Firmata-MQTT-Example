27a0ca3add29da35f7d86c65f1e0bf4a1d6f3bff
WGLabz <oksbwn@gmail.com>
2020-05-01 00:39:12 +0530

    Hello

--- Example Codes/mqtt.py
+++ Example Codes/mqtt.py
@@ -1,16 +1,15 @@
 import paho.mqtt.client as mqtt
+import time
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+    client.subscribe("home/test/in")
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+
 
 client = mqtt.Client()
 client.username_pw_set("********", password="**")
@@ -19,8 +18,8 @@ client.on_message = on_message
 
 client.connect("192.168.0.7", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+client.loop_start()
+
+while 1:
+    client.publish("home/test/out", "Test Data")
+    time.sleep(5)
